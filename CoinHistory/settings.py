afa6c068b1797765fcd9f3fd210dfdcfdd865e72
rubix0307 <miroshnichenkoartem0307@gmail.com>
2024-01-19 14:22:51 +0100

    add auth form & static

--- CoinHistory/settings.py
+++ CoinHistory/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -125,8 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'CoinHistory', 'static'),]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'profile'
---
+++ users/views.py
@@ -0,0 +1,7 @@
+from django.core.handlers.wsgi import WSGIRequest
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def profile(request: WSGIRequest):
+    return render(request, 'users/profile.html')
