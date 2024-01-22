dd58a8e4b5a4a134ce9db3e481eb4e25e3ce1400
rubix0307 <miroshnichenkoartem0307@gmail.com>
2024-01-23 00:58:12 +0100

    remove platform by foreign key

--- currency/models.py
+++ currency/models.py
@@ -6,15 +6,6 @@ class Tag(models.Model):
     name = models.CharField(max_length=255)
     slug = models.CharField(max_length=255)
 
-class Platform(models.Model):
-    # e.g. EOS, NEO, Ethereum, proprietary blockchain
-    name = models.CharField(max_length=255)
-    slug = models.CharField(max_length=255)
-    symbol = models.CharField(max_length=255)
-
-class Url(models.Model):
-    url = models.URLField(max_length=1000)
-
 class Currency(models.Model):
     id = models.PositiveIntegerField(primary_key=True)
     name = models.CharField(max_length=255) # e.g. Bitcoin
@@ -23,12 +14,11 @@ class Currency(models.Model):
     symbol = models.CharField(max_length=255) # e.g. BTC
     logo = models.URLField(max_length=1000)
     coinmarketcap_url = models.URLField(max_length=1000)
-    circulating_supply = models.PositiveBigIntegerField() # coins that are circulating in the market and in the general public's hands
+    circulating_supply = models.PositiveBigIntegerField(null=True) # coins that are circulating in the market and in the general public's hands
     total_supply = models.DecimalField(max_digits=20, decimal_places=0, null=True) # coins in existence right now minus any coins that have been verifiably burned (optional)
     max_supply = models.PositiveBigIntegerField(null=True) # maximum of coins that will ever exist in the lifetime of the asset
     tags = models.ManyToManyField(Tag)
-    platform = models.ForeignKey(Platform, on_delete=models.CASCADE, null=True)
-    urls = models.ForeignKey(Url, on_delete=models.CASCADE, null=True)
+    platform = models.CharField(max_length=255, null=True)
     last_updated = models.DateTimeField(default=timezone.now)
     date_added = models.DateTimeField(default=timezone.now)
 
--- currency/views.py
+++ currency/views.py
@@ -2,17 +2,17 @@ from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render
 from django.utils import timezone
 from temp.coin_api import get_latest
-from .models import Currency, Url, Platform, Tag
+from .models import Currency, Tag
 from django.db.utils import IntegrityError
 
 def index(request: WSGIRequest):
     context = {
+        'currencies': Currency.objects.order_by('-date_added').all()[:1000]
     }
     return render(request, 'currency/index.html', context=context)
 
 def temp_func(request: WSGIRequest):
     currency_fields = [field.name for field in Currency._meta.get_fields()]
-    platform_fields = [field.name for field in Platform._meta.get_fields()]
     data = get_latest().get('data')
 
     for crypt_data in data:
@@ -28,19 +28,6 @@ def temp_func(request: WSGIRequest):
 
         currency_data = {key: value for key, value in crypt_data.items() if key in currency_fields}
         currency_data.pop('tags')
-
-        platform_data = {key: value for key, value in (crypt_data.get('platform') or {}).items() if key in platform_fields}
-        if platform_data:
-
-            try:
-                platform, created = Platform.objects.get_or_create(**platform_data)
-            except Exception as ex:
-                platform = Platform.objects.filter(id=platform_data['id']).first()
-            finally:
-                currency_data.update({'platform': platform})
-
-        else:
-            currency_data.pop('platform')
 
         currency, created = Currency.objects.update_or_create(defaults=currency_data, id=currency_data['id'])
         currency.tags.set(all_tag_objects)
