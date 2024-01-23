c4fb4cdcb0d45497c109f92e78a2f1458d7035a6
rubix0307 <miroshnichenkoartem0307@gmail.com>
2024-01-23 01:00:27 +0100

    add scraper for new listings

--- CoinHistory/settings.py
+++ CoinHistory/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 load_dotenv('.env')
+load_dotenv(os.path.join('..', '..', '.env'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
